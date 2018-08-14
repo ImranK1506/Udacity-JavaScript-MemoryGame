2edba66d6fee87e78bafd3096a007776d5c0871d
Imran Khan <imran.khan@kpn.com>
2018-08-14 15:01:20 +0200

    can now match and unmatch cards

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 /*
  * Create a list that holds all of your cards
  */
-
 const cardList = [
      'fa fa-diamond',
      'fa fa-diamond',
@@ -29,77 +28,26 @@ console.dir(cardList);
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
-
 const deck = document.querySelector('.deck');
 
 // Store clicked card in an empty array
 let openedCards = [];
 let matchedCards = [];
 
-// Loop over the cardList
-for (let i = 0; i < cardList.length; i++) {
-    const card = document.createElement('li');
-    card.classList.add('card');
+function startGame() {
+    for (let i = 0; i < cardList.length; i++) {
+        const card = document.createElement('li');
+        card.classList.add('card');
 
-// add card content to the list using back tick or template literals
-    card.innerHTML = `<i class='${cardList[i]}'></i>`;
-// append cardList to the parent with the card argument
-    deck.appendChild(card);
+    // add card content to the list using back tick or template literals
+        card.innerHTML = `<i class='${cardList[i]}'></i>`;
+    // append cardList to the parent with the card argument
+        deck.appendChild(card);
 
-// add event listener with click on each card
-    card.addEventListener('click', function() {
-
-        const currentCard = this;
-        const previousCard = openedCards[0];
-        // opened cards
-        if (openedCards.length === 1) {
-            // add open and show class than push cardList to the empty array
-            card.classList.add('open', 'show');
-            openedCards.push(this);
-
-            // compare cards
-            if (currentCard.innerHTML === previousCard.innerHTML) {
-
-                currentCard.classList.add('match');
-                previousCard.classList.add('match');
-
-                matchedCards.push(currentCard, previousCard);
-                // reset opened cards history
-                openedCards = [];
-
-                console.log('Match');
-
-                // Game end after all cards are matched
-                endGame();
-
-            } else {
-                // Gives time to see the card even when not a match
-                setTimeout(function() {
-                    currentCard.classList.remove('open', 'show');
-                    previousCard.classList.remove('open', 'show');
-                    console.log('No match');
-                    // reset opened cards history
-                    openedCards = [];
-                }, 700);
-            }
-
-        } else {
-        // unopened cards
-            card.classList.add('open', 'show');
-            openedCards.push(this);
-        }
-    })
-}
-
-/*
- * Game end
- */
-function endGame() {
-    if (matchedCards.length === cardList.length) {
-        alert('Good Game!');
+    // click event on cards
+        onClick(card);
     }
 }
-
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 // function shuffle(array) {
@@ -127,3 +75,69 @@ function endGame() {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+function onClick(card) {
+    card.addEventListener('click', function() {
+
+        const currentCard = this;
+        const previousCard = openedCards[0];
+
+        // opened cards
+        if (openedCards.length === 1) {
+            // add open and show class than push cardList to the empty array
+            card.classList.add('open', 'show', 'disable');
+            openedCards.push(this);
+
+            // compare cards
+            compareCards(currentCard, previousCard);
+
+        } else {
+            // unopened cards
+            card.classList.add('open', 'show', 'disable');
+            openedCards.push(this);
+        }
+    })
+}
+
+/*
+ * Compare cards
+ */
+function compareCards(currentCard, previousCard) {
+    if (currentCard.innerHTML === previousCard.innerHTML) {
+
+        currentCard.classList.add('match');
+        previousCard.classList.add('match');
+
+        matchedCards.push(currentCard, previousCard);
+        // reset opened cards history
+        openedCards = [];
+
+        console.log('Match');
+
+        // Game end after all cards are matched
+        endGame();
+
+        } else {
+        // Gives time to see the cards even when there is no match
+        setTimeout(function() {
+            currentCard.classList.remove('open', 'show', 'disable');
+            previousCard.classList.remove('open', 'show', 'disable');
+            console.log('No match');
+            // reset opened cards history
+            openedCards = [];
+        }, 700);
+    }
+}
+
+/*
+ * Start game
+ */
+startGame();
+
+/*
+ * Game end
+ */
+function endGame() {
+    if (matchedCards.length === cardList.length) {
+        alert('Good Game!');
+    }
+}
